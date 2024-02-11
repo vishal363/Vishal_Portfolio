5eeebf9d971f2839a321a1dfa33129f2d0cad516
Vishal Brahmananda <71640603+vishal363@users.noreply.github.com>
2024-02-11 12:00:17 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,21 +11,21 @@
     });
 
 
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
+    // // Smooth scrolling on the navbar links
+    // $(".navbar-nav a").on('click', function (event) {
+    //     if (this.hash !== "") {
+    //         event.preventDefault();
             
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
+    //         $('html, body').animate({
+    //             scrollTop: $(this.hash).offset().top - 45
+    //         }, 1500, 'easeInOutExpo');
             
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
+    //         if ($(this).parents('.navbar-nav').length) {
+    //             $('.navbar-nav .active').removeClass('active');
+    //             $(this).closest('a').addClass('active');
+    //         }
+    //     }
+    // });
 
 
     // Typed Initiate
